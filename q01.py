cb6e7424624d73471551a631e018ea69feb5e487
Jairo Hernando Nieto Hernández <jnietoh@unal.edu.co>
2019-05-31 12:37:04 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,3 +1,10 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
+data = open('data.csv', 'r'). readlines()           #Lee el archivo
+data = [row[0:-1] for row in data]                  #Elimina los retornos del carro   
+data = [x.replace('\t', ',') for x in data]         #Reemplaza los separador "\t" por ","
+data = [row.split(',') for row in data]             #Separa los elementos de las listas por ","
+for i in data:                                      #Se crea ciclo for
+    total = sum(float(i[1]) for i in data)          #Almacena la suma la segunda fila del archivo en la variable "total"
+print (total)                                       #Imprime la svariable total
