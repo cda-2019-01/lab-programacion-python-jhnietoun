4df7d60bd93b15839698ff6718e140129e41619c
Jairo Hernando Nieto Hernández <jnietoh@unal.edu.co>
2019-06-07 07:15:56 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -7,4 +7,4 @@ data = [x.replace('\t', ',') for x in data]         #Reemplaza los separador "\t
 data = [row.split(',') for row in data]             #Separa los elementos de las listas por ","
 for i in data:                                      #Se crea ciclo for
     total = sum(float(i[1]) for i in data)          #Almacena la suma la segunda fila del archivo en la variable "total"
-print (total)                                       #Imprime la svariable total
+print (total)                                       #Imprime la variable total
